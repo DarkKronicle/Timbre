ffaf9bcc9c7ac2f7857cadef0913a2f67d34265a
DarkKronicle <darkkronicle@gmail.com>
2022-07-25 19:42:05 -0600

    Insantiating new spotify for each request

--- backend/launch.py
+++ backend/launch.py
@@ -18,8 +18,6 @@ def run():
     tk.client_secret_var = config['client_secret']
     tk.redirect_uri_var = config['redirect_uri']
 
-    sp = tk.Spotify(asynchronous=True, chunked_on=True, max_limits_on=True)
-    cred = tk.Credentials(config['client_id'], config['client_secret'], config['redirect_uri'], asynchronous=True)
     back = server.Server(config['backend']['websocket_port'])
     asyncio.run(back.run_server())
 
--- backend/spotify_request.py
+++ backend/spotify_request.py
@@ -1,8 +1,13 @@
 import generator
-from launch import sp
+import tekore as tk
+
+
+def create_spotify():
+    return tk.Spotify(asynchronous=True, chunked_on=True, max_limits_on=True)
 
 
 async def run_instruction(token: str, data: dict):
+    sp = create_spotify()
     with sp.token_as(token):
         tracks = await generator.instruction.run(sp, data)
     return tracks
